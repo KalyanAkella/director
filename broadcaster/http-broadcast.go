1fe842f5f2066b82f446d4a20630cf2978124e2f
Kalyan Akella <kalyan.akella@gmail.com>
2018-05-25 17:54:50 +0530

    Introduced the broadcaster package and tests

---
+++ broadcaster/http-broadcast.go
@@ -0,0 +1,210 @@
+package broadcaster
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type (
+	BroadcastOption  = byte
+	BroadcastOptions = map[BroadcastOption]string
+	EndPointId       = string
+	EndPoint         = *url.URL
+	EndPoints        = map[EndPointId]EndPoint
+	BroadcastConfig  = struct {
+		Options  BroadcastOptions
+		Backends EndPoints
+	}
+)
+
+const (
+	PORT BroadcastOption = iota
+	PRIMARY
+	RESPONSE_TIMEOUT_IN_SECS
+)
+
+func broadcastError(msg string) error {
+	return fmt.Errorf("[HTTP Broadcast] %s", msg)
+}
+
+func validate(config *BroadcastConfig) error {
+	if config == nil {
+		return broadcastError("Configuration for broadcast must be provided")
+	}
+	if config.Options == nil {
+		return broadcastError("Broadcast options are missing")
+	}
+	if _, present := config.Options[PORT]; !present {
+		return broadcastError("Broadcast port is missing in broadcast options")
+	}
+	if _, present := config.Options[PRIMARY]; !present {
+		return broadcastError("Primary endpoint is missing in broadcast options")
+	}
+	if _, present := config.Options[RESPONSE_TIMEOUT_IN_SECS]; !present {
+		return broadcastError("Response timeout is missing in broadcast options")
+	}
+	if _, err := strconv.Atoi(config.Options[RESPONSE_TIMEOUT_IN_SECS]); err != nil {
+		return broadcastError(err.Error())
+	}
+	if config.Backends == nil || len(config.Backends) == 0 {
+		return broadcastError("Backends are missing or empty")
+	}
+	if _, present := config.Backends[config.Options[PRIMARY]]; !present {
+		return broadcastError("Primary backend missing from the given set of backends")
+	}
+	for k, v := range config.Backends {
+		if v == nil {
+			return broadcastError(fmt.Sprintf("Backend endpoint with ID: %s does not have any associated data", k))
+		}
+	}
+	return nil
+}
+
+func cloneHeader(h http.Header) http.Header {
+	h2 := make(http.Header, len(h))
+	for k, vv := range h {
+		vv2 := make([]string, len(vv))
+		copy(vv2, vv)
+		h2[k] = vv2
+	}
+	return h2
+}
+
+var (
+	logger  = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog = func(msg string) {
+		logger.SetPrefix("INFO:")
+		logger.Println(msg)
+	}
+	errorLog = func(msg string) {
+		logger.SetPrefix("ERROR:")
+		logger.Println(msg)
+	}
+	// Hop-by-hop headers. These are removed when sent to the backend.
+	// http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
+	hopHeaders = []string{
+		"Connection",
+		"Proxy-Connection", // non-standard but still sent by libcurl and rejected by e.g. google
+		"Keep-Alive",
+		"Proxy-Authenticate",
+		"Proxy-Authorization",
+		"Te",      // canonicalized version of "TE"
+		"Trailer", // not Trailers per URL above; http://www.rfc-editor.org/errata_search.php?eid=4522
+		"Transfer-Encoding",
+		"Upgrade",
+	}
+)
+
+func newRequest(req *http.Request, req_url *url.URL) *http.Request {
+	new_req := req.WithContext(context.Background())
+
+	if req.ContentLength == 0 {
+		new_req.Body = nil
+	}
+	new_req.Header = cloneHeader(req.Header)
+	new_req.URL = req_url
+	new_req.Close = false
+
+	// Remove hop-by-hop headers listed in the "Connection" header.
+	// See RFC 2616, section 14.10.
+	if c := new_req.Header.Get("Connection"); c != "" {
+		for _, f := range strings.Split(c, ",") {
+			if f = strings.TrimSpace(f); f != "" {
+				new_req.Header.Del(f)
+			}
+		}
+	}
+
+	// Remove hop-by-hop headers to the backend. Especially
+	// important is "Connection" because we want a persistent
+	// connection, regardless of what the client sent to us.
+	for _, h := range hopHeaders {
+		if new_req.Header.Get(h) != "" {
+			new_req.Header.Del(h)
+		}
+	}
+	return new_req
+}
+
+func roundTrip(req *http.Request, transport http.RoundTripper, res_chan chan<- *http.Response, err_chan chan<- error) {
+	res, err := transport.RoundTrip(req)
+	if err == nil {
+		res_chan <- res
+	} else {
+		err_chan <- err
+	}
+}
+
+func readResponseTimeout(config *BroadcastConfig) time.Duration {
+	n, _ := strconv.Atoi(config.Options[RESPONSE_TIMEOUT_IN_SECS])
+	return time.Duration(n) * time.Second
+}
+
+func broadcastHandler(config *BroadcastConfig) http.HandlerFunc {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+
+		res_chan := make(chan *http.Response)
+		err_chan := make(chan error)
+
+		transport := http.DefaultTransport
+		primary_endpoint_id := config.Options[PRIMARY]
+		for id, endpoint := range config.Backends {
+			request := newRequest(req, endpoint)
+			switch id {
+			case primary_endpoint_id:
+				go roundTrip(request, transport, res_chan, err_chan)
+			default:
+				go func() {
+					if res, err := transport.RoundTrip(request); err != nil {
+						errorLog(fmt.Sprintf("Error response from [%s]:[%s] -> %s", id, endpoint, err.Error()))
+					} else {
+						defer res.Body.Close()
+						if r, e := ioutil.ReadAll(res.Body); e != nil {
+							errorLog(e.Error())
+						} else {
+							infoLog(string(r))
+						}
+					}
+				}()
+			}
+		}
+
+		response_timeout := readResponseTimeout(config)
+		select {
+		case res := <-res_chan:
+			defer res.Body.Close()
+			r, e := ioutil.ReadAll(res.Body)
+			if e == nil {
+				fmt.Fprintln(rw, string(r))
+			} else {
+				fmt.Fprintln(rw, string(e.Error()))
+			}
+		case err := <-err_chan:
+			fmt.Fprintln(rw, string(err.Error()))
+		case <-time.After(response_timeout):
+			fmt.Fprintln(rw, "Timeout")
+		}
+	})
+}
+
+func BroadcastHTTPHandler(config *BroadcastConfig) (http.HandlerFunc, error) {
+	if err := validate(config); err != nil {
+		return nil, err
+	}
+	return broadcastHandler(config), nil
+}
+
+func ServeOnHTTP(config *BroadcastConfig) error {
+	if err := validate(config); err != nil {
+		return err
+	}
+	return http.ListenAndServe(fmt.Sprintf(":%s", config.Options[PORT]), broadcastHandler(config))
+}
---
+++ broadcaster/http-broadcast_test.go
@@ -0,0 +1,136 @@
+package broadcaster
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func BenchmarkHTTPBroadcast(b *testing.B) {
+	b.ResetTimer()
+	// b.RunParallel(...)
+	for i := 0; i < b.N; i++ {
+	}
+}
+
+func asUrl(urlString string) *url.URL {
+	aUrl, err := url.Parse(urlString)
+	if err != nil {
+		panic(err)
+	} else {
+		return aUrl
+	}
+}
+
+func newListener(endpoint string) net.Listener {
+	if l, err := net.Listen("tcp", endpoint); err != nil {
+		panic(err)
+	} else {
+		return l
+	}
+}
+
+func newBroadcastServer(handler http.HandlerFunc) *httptest.Server {
+	aServer := httptest.NewUnstartedServer(handler)
+	aServer.Listener = newListener(fmt.Sprintf("localhost:%s", BroadcastServerPort))
+	aServer.Start()
+	return aServer
+}
+
+func newServer(tag, endpoint string, res_chan chan<- string) *httptest.Server {
+	aServer := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		response := fmt.Sprintf("%s Got Request", tag)
+		res_chan <- response
+		fmt.Fprint(w, response)
+	}))
+	aServer.Listener = newListener(endpoint)
+	aServer.Start()
+	return aServer
+}
+
+const (
+	PrimaryTag          = "B2"
+	BroadcastServerPort = "9090"
+	NumRequests         = 10
+)
+
+var backendServers = map[string]string{
+	"B1":       "localhost:9091",
+	PrimaryTag: "localhost:9092",
+	"B3":       "localhost:9093",
+}
+
+func readTag(response string) string {
+	var tag string
+	if _, err := fmt.Sscanf(response, "%s", &tag); err != nil {
+		panic(err)
+	}
+	return tag
+}
+
+func httpGet(url string) string {
+	if res, err := http.Get(url); err != nil {
+		panic(err)
+	} else {
+		defer res.Body.Close()
+		if res_bytes, err := ioutil.ReadAll(res.Body); err != nil {
+			panic(err)
+		} else {
+			return string(res_bytes)
+		}
+	}
+}
+
+func TestHTTPBroadcast(t *testing.T) {
+	// Given
+	res_chan := make(chan string, len(backendServers))
+	backends := make(map[EndPointId]EndPoint)
+	for t, e := range backendServers {
+		server := newServer(t, e, res_chan)
+		defer server.Close()
+		backends[t] = asUrl(fmt.Sprintf("http://%s", e))
+	}
+
+	// When
+	if broadcast_handler, err := BroadcastHTTPHandler(&BroadcastConfig{
+		Backends: backends,
+		Options: map[BroadcastOption]string{
+			PORT:                     BroadcastServerPort,
+			PRIMARY:                  PrimaryTag,
+			RESPONSE_TIMEOUT_IN_SECS: "10",
+		},
+	}); err != nil {
+		t.Fatal(err)
+	} else {
+		broadcast_server := newBroadcastServer(broadcast_handler)
+		defer broadcast_server.Close()
+	}
+
+	// Then
+	responded := make(map[string]int, len(backendServers))
+	for t := range backendServers {
+		responded[t] = 0
+	}
+	for i := 1; i <= NumRequests; i++ {
+		broadcast_res := httpGet("http://localhost:9090")
+		if primary_tag := readTag(broadcast_res); primary_tag != PrimaryTag {
+			t.Errorf("Expected primary tag %s, Actual primary tag %s. Broadcast Response %s", PrimaryTag, primary_tag, broadcast_res)
+		}
+		for range backendServers {
+			select {
+			case msg := <-res_chan:
+				responded[readTag(msg)]++
+			default:
+			}
+		}
+	}
+	for k, v := range responded {
+		if v < NumRequests {
+			t.Errorf("No response from server with tag: %s", k)
+		}
+	}
+}
