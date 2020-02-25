9f0a3059fb0cc46065c997af16b232f769a641c1
Kalyan Akella <kalyan.akella@gmail.com>
2020-02-25 17:31:02 +0530

    Migrating to modules

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KalyanAkella/director
+
+go 1.13
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"go-broadcaster/broadcaster"
 	"log"
+
+	"github.com/KalyanAkella/director/broadcaster"
 )
 
 func main() {
